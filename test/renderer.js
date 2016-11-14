43e6d7b1bc006627808106660940eda5baea0b75
Sean Genabe <seangenabe@outlook.com>
2016-11-15 00:44:54 +0800

    tests; fixed call to renderSingle

--- test/renderer.js
+++ test/renderer.js
@@ -2,26 +2,34 @@ const t = require('ava')
 const Renderer = require('..').Renderer
 const bel = require('bel')
 
+function toHTMLString(result) {
+  let testElement = bel`<span>${result}</span>`
+  let ret = testElement.toString()
+  ret = ret.substring(6, ret.length - 7)
+  return ret
+}
+
 t('render should do nothing', t => {
   let r = new Renderer()
-  t.deepEqual(r.render(['abc', 'def']), ['abc', 'def'])
+  t.is(toHTMLString(r.render(['abc', 'def'])), 'abcdef')
 })
 
 t('render should return empty', t => {
   let r = new Renderer()
-  t.deepEqual(r.render(), [])
+  t.is(toHTMLString(r.render()), '')
 })
 
 t('basic render', t => {
   let r = new Renderer()
-  let content = r.render(['a', { rb: '今日', rt: 'きょう' }, 'b'])
-  let testElement = bel`<span>${content}</span>`
-  t.is(testElement.toString(), '<span>a<ruby>今日<rp>(</rp><rt>きょう</rt><rp>)</rp></ruby>b</span>')
+  t.is(
+    toHTMLString(r.render(['a', { rb: '今日', rt: 'きょう' }, 'b'])),
+    'a<ruby>今日<rp>(</rp><rt>きょう</rt><rp>)</rp></ruby>b'
+  )
 })
 
 t('rb without rt', t => {
   let r = new Renderer()
-  t.deepEqual(r.render(['a', { rb: 'b' }, 'c']), ['a', 'b', 'c'])
+  t.is(toHTMLString(r.render(['a', { rb: 'b' }, 'c'])), 'abc')
 })
 
 t('throw error', t => {
@@ -31,7 +39,5 @@ t('throw error', t => {
 
 t('should render valid HTML', t => {
   let r = new Renderer()
-  let content = r.render(['&><'])
-  let testElement = bel`<span>${content}</span>`
-  t.is(testElement.toString(), '<span>&amp;&gt;&lt;</span>')
+  t.is(toHTMLString(r.render(['&><'])), '&amp;&gt;&lt;')
 })
